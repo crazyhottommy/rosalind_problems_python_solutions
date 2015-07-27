8effaef3726a1d0260c7932a67e63b99d096cf1e
crazyhottommy <tangming2005@gmail.com>
2015-07-27 17:27:00 -0500

    transcribe dna to rna added

---
+++ Transcribing_DNA_into_RNA.py
@@ -0,0 +1,9 @@
+
+# change Ts in DNA to Us in RNA
+def transcribe_dna_rna(dna):
+    rna = dna.replace("T", "U")
+    return rna
+
+
+# input: GATGGAACTTGACTACGTAAATT
+# output: GAUGGAACUUGACUACGUAAAUU
