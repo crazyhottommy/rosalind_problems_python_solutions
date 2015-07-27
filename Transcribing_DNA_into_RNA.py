67cb1edaa64887e5911b641f041854b95d47d8c6
Ming Tang <tangming2005@gmail.com>
2015-07-27 17:30:50 -0500

    Delete Transcribing_DNA_into_RNA.py

--- Transcribing_DNA_into_RNA.py
+++
@@ -1,9 +0,0 @@
-
-# change Ts in DNA to Us in RNA
-def transcribe_dna_rna(dna):
-    rna = dna.replace("T", "U")
-    return rna
-
-
-# input: GATGGAACTTGACTACGTAAATT
-# output: GAUGGAACUUGACUACGUAAAUU
